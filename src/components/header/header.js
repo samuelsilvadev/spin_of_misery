e72ca08bd11a079509356ded3e8a120e7f51b938
Kyle Carney <kyle.e.carney@gmail.com>
2018-10-13 12:55:57 -0400

    update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,10 +4,10 @@ import FadeIn from '../transitions/fade-in';
 const Header = () => { return(
     <header className="App-header">
         <FadeIn in={true} duration={600} length={'30px'} direction={'bottom'}>
-            <h2> </h2>
+            <h2>Spin the wheel and face the consequences at your own risk</h2>
         </FadeIn>
         <FadeIn in={true} duration={600} length={'50px'} direction={'bottom'} >
-            <h1>Spin of Misery</h1>
+            <h1>Wheel of Misery</h1>
         </FadeIn>
     </header>
 )};
