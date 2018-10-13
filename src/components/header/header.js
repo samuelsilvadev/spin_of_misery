b384285da76ebb1669a41786202b5e300030943f
Kyle Carney <kyle.e.carney@gmail.com>
2018-10-13 12:50:36 -0400

    update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,10 +4,10 @@ import FadeIn from '../transitions/fade-in';
 const Header = () => { return(
     <header className="App-header">
         <FadeIn in={true} duration={600} length={'30px'} direction={'bottom'}>
-            <h2>2018 Penn LPS Coding Boot Camp</h2>
+            <h2> </h2>
         </FadeIn>
         <FadeIn in={true} duration={600} length={'50px'} direction={'bottom'} >
-            <h1>Philly Players Clicky Game</h1>
+            <h1>Spin of Misery</h1>
         </FadeIn>
     </header>
 )};
